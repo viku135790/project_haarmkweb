3023a010643dc903bc3c593f5be2c20a2ceb522d
Brajkishor Tomar <B.ktomar98@gmail.com>
2023-06-16 14:57:14 +0530

    contact-page

---
+++ companywebsite/src/root-folder/allcomponents/contact/ContactMid.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./ContactMid.css"
+
+const ContactMid = () => {
+  return (
+    <div className="contact-us-mid">
+        <div className="contact-us-mid1">I’m Looking For</div>
+        <div className="contact-us-mid2">
+        Please select your area of interest below. An Infosys representative will contact you shortly after receiving your request.
+        </div>
+    </div>
+  )
+};
+
+export default ContactMid
